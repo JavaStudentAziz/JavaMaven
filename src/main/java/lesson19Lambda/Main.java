47c5d6a80288c834a8adf52c4da2e2a56844696e
huskk <diarmaittsss@gmail.com>
2023-05-29 21:03:12 +0300

    lesson19

---
+++ src/main/java/lesson19Lambda/Main.java
@@ -0,0 +1,69 @@
+package lesson19Lambda;
+
+import ApiLesson.Person;
+import ApiLesson.PersonApi;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+   public static void main(String[] args) {
+
+      List<Person> personList = PersonApi.getPersonList(10);
+
+      for (Person person : personList) {
+	 System.out.println(person);
+      }
+
+      System.out.println("___________________________________________________________________________________________");
+
+      LambdaExample.sortLambda(personList, new Predicato<Person>() {
+	 @Override
+	 public boolean test(Person person) {
+	    return LocalDateTime.now().getYear()
+		    - person.getDateOfBirthday().getYear() < 30;
+	 }
+      });
+      System.out.println("___________________________________________________________________________________________");
+
+      // слева от стрелки то, что мы передаем
+      // -> - магическая стрелка
+      // справа результат
+
+      LambdaExample.sortLambda(personList,
+	      (Person person) -> {
+		 System.out.println("func checking");
+		 return person.getCountry().equals("Spain");
+	      });
+
+      System.out.println("___________________________________________________________________________________________");
+
+      LambdaExample.sortLambda(personList,
+	      p -> p.getCountry().equals("Spain"));
+
+      // слева 1 тип - можно без скобок
+      // больше 1 типо - закрывать в скобки
+      // ->
+      // если ставим фигурные скобки - должен быть return
+      // фигурные скобки ставятся в случаях если есть больше 1 действия
+
+   }
+}
+
+//Интерфейс только с абстрактным 1 методом называется функциональным интерфейсом
+@FunctionalInterface
+interface Predicato<T> {
+   boolean test(T t);
+
+}
+
+class LambdaExample {
+   public static void sortLambda(List<Person> personList, Predicato<Person> p) {
+
+      for (Person person : personList) {
+	 if (p.test(person)) System.out.println(person);
+      }
+
+   }
+
+}
